3b86dd56f1e3bf14f725694d2cec72533f9ca28b
«dima» <dima-titov-93@mail.ru>
2023-09-30 17:49:50 +0300

    Initial commit

---
+++ src/Task01.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Task01 {
+    public static void main(String[] args) {
+        boolean repeat = true;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while (repeat){
+            System.out.print("Введите число: ");
+            try{
+                // Проверка на тип float
+                Float number = Float.parseFloat(reader.readLine());
+                System.out.println("Введённое число : " + number);
+                repeat = false;
+            } catch (IOException | NumberFormatException e) {
+                System.out.println("Неверный ввод данных.");;
+            }
+        }
+    }
+}
+
---
+++ src/Task04.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Task04 {
+    public static void main(String[] args) throws IllegalAccessException {
+        Scanner scanner = new Scanner(System.in);
+        boolean repeat = true;
+        System.out.print("Введите текст: ");
+        String text = scanner.nextLine();
+
+        if (text.isEmpty()) {
+            throw new IllegalAccessException("Пустая строка! Повторите ввод!");
+        } else {
+            System.out.println("Текст: " + text);
+            repeat = false;
+        }
+
+    }
+}
